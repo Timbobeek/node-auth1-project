30812faa1d593fdd475718230fa174f9c91b1bf2
Tim Goloshchapov <tim.goloschapov@gmail.com>
2022-02-21 02:03:05 -0600

    passwords hashed!

--- api/auth/auth-router.js
+++ api/auth/auth-router.js
@@ -2,6 +2,8 @@
 // middleware functions from `auth-middleware.js`. You will need them here!
 
 const router = require('express').Router()
+const bcrypt = require('bcryptjs')
+const User = require('../users/users-model')
 const {
   checkPasswordLength,
   checkUsernameExists,
@@ -32,7 +34,14 @@ const {
   }
  */
 router.post('/register', checkPasswordLength, checkUsernameFree, (req,res,next)=>{
-  res.json('register')
+  const { username, password} = req.body
+  const hash = bcrypt.hashSync(password, 8)
+
+  User.add({ username, password: hash})
+    .then(saved => {
+      res.status(201).json(saved)
+    })
+    .catch(next)
 })
 
 
